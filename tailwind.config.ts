f9c07429a8c65169cf2db451ab9d90c4bfd7ad5e
Moalosi Liteboho <litebohojamesmoalosi@gmail.com>
2024-06-17 08:53:16 +0200

    add changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import {nextui} from "@nextui-org/react";
 
 const config: Config = {
   content: [
@@ -8,9 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
-  darkMode: "class",
-  plugins: [nextui()]
+  plugins: [],
 };
 export default config;
